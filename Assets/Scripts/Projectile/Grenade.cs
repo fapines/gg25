32c48c2c91355990726aa335966e0e533dde3b7c
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-01-24 02:38:25 +0330

    Add Bullets Prefab

---
+++ Assets/Scripts/Projectile/Grenade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityStandardAssets._2D;
+
+public class Grenade : Projectile
+{
+    public GameObject explosion;
+    private void OnDestroy()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
