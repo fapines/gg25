24ee0a5708c142247706f89aabd32fae54ae53f0
Mehran Fallah <M.Mehran.Fallah@gmail.com>
2025-01-24 03:01:51 +0330

    Add Explosion and Grenade Prefabs

---
+++ Assets/Scripts/Projectile/Explosion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class Explosion : MonoBehaviour
+{
+    public int damage;
+    public float destroyAfterSecs = 3f;
+    private void Start()
+    {
+        Destroy(gameObject, destroyAfterSecs);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        PlatformerCharacter2D enemy = other.GetComponent<PlatformerCharacter2D>();
+        RaycastHit2D hitWhat = Physics2D.Raycast(transform.position, other.offset);
+        if (enemy = null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+        if (hitWhat)
+        {
+            Debug.Log(hitWhat.transform.name);
+        }
+    }
+
+    /* private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, 4);
+    }*/
+}
