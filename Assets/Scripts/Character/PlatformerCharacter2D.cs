4b3fe4f7df45940297445fcec64c62026b6d610f
fapines <behnam.gholizadeh76@gmail.com>
2025-01-24 00:26:52 +0330

    ADD character movments
    ADD character movments
    ADD pause menu
    ADD main menu

---
+++ Assets/Scripts/Character/PlatformerCharacter2D.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class PlatformerCharacter2D : MonoBehaviour
+    {
+        [SerializeField] private float m_MaxSpeed = 10f; // Max speed in the x axis
+        [SerializeField] private float m_JumpForce = 400f; // Jump force
+        [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f; // Crouch speed multiplier
+        [SerializeField] private bool m_AirControl = false; // Allow air control?
+        [SerializeField] private LayerMask m_WhatIsGround; // Ground layer mask
+
+        private Transform m_GroundCheck; // Ground check position
+        private bool m_Grounded; // Is the character grounded?
+        private Transform m_CeilingCheck; // Ceiling check position
+        private bool m_FacingRight = true; // Is the character facing right?
+        private Rigidbody2D m_Rigidbody2D; // Rigidbody2D component
+
+        public GameObject bulletPrefab; // Bullet prefab
+        public GameObject grenadePrefab; // Grenade prefab
+        public Transform weaponLocation; // Weapon location
+        public Vector2 aimingDirection { get; set; } // Direction for aiming
+
+        public float fireRate = 0.5f; // Fire rate for shooting
+        private float nextFire = 0.0f; // Time of next allowed fire
+        public int health = 100; // Player health
+
+        private void Awake()
+        {
+            m_GroundCheck = transform.Find("GroundCheck");
+            m_CeilingCheck = transform.Find("CeilingCheck");
+            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            m_Grounded = Physics2D.OverlapCircle(m_GroundCheck.position, 0.2f, m_WhatIsGround);
+        }
+
+        public void Move(float move, bool crouch, bool jump)
+        {
+            if (m_Grounded || m_AirControl)
+            {
+                // Move character horizontally
+                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+                // Flip character based on movement direction
+                if (move > 0 && !m_FacingRight)
+                {
+                    Flip();
+                }
+                else if (move < 0 && m_FacingRight)
+                {
+                    Flip();
+                }
+            }
+
+            if (m_Grounded && jump)
+            {
+                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce)); // Apply jump force
+            }
+        }
+
+        private void Flip()
+        {
+            m_FacingRight = !m_FacingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+        public void shoot(bool shoot)
+        {
+            if (shoot && Time.time > nextFire)
+            {
+                if (aimingDirection == Vector2.zero)
+                {
+                    aimingDirection = new Vector2(1, 0); // Default aiming direction
+                }
+
+                if (aimingDirection.x > 0 && !m_FacingRight)
+                {
+                    Flip();
+                }
+                else if (aimingDirection.x < 0 && m_FacingRight)
+                {
+                    Flip();
+                }
+
+                nextFire = Time.time + fireRate;
+                GameObject bullet = Instantiate(bulletPrefab, weaponLocation.position, weaponLocation.rotation);
+                bullet.GetComponent<Bullet>().Fire(aimingDirection); // Fire the bullet in the direction of the mouse
+            }
+        }
+
+        public void shootGrenade(bool shootGrenade)
+        {
+            if (shootGrenade && Time.time > nextFire)
+            {
+                if (aimingDirection.x > 0 && !m_FacingRight)
+                {
+                    Flip();
+                }
+                else if (aimingDirection.x < 0 && m_FacingRight)
+                {
+                    Flip();
+                }
+
+                nextFire = Time.time + fireRate;
+                GameObject grenade = Instantiate(grenadePrefab, weaponLocation.position, weaponLocation.rotation);
+                grenade.GetComponent<Grenade>().Fire(aimingDirection); // Fire the grenade in the direction of the mouse
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject); // Destroy the character
+        }
+    }
+}
--- Assets/Scripts/Input/Player.cs
+++ Assets/Scripts/Input/Player.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityStandardAssets._2D;
 
-// This class reads the input from player and feeds it to the corresponding CharacterController
 public class Player : MonoBehaviour
 {
-    // Public fields
     public PlatformerCharacter2D characterController;
 
     // Player input by input system
@@ -24,6 +22,8 @@ public class Player : MonoBehaviour
     private bool shooting = false;
     private bool shootingGrenade = false;
 
+    private Camera mainCamera; // Reference to the main camera for getting mouse position
+
     private void Awake()
     {
         movement.performed += OnMovementPerformed;
@@ -35,9 +35,8 @@ public class Player : MonoBehaviour
         shootGrenade.performed += OnShootGrenadePerformed;
         shootGrenade.canceled += OnShootGrenadePerformed;
         aim.performed += OnAimPerformed;
-        //aim.canceled += OnAimPerformed;
 
-
+        mainCamera = Camera.main; // Assign the main camera
     }
 
     private void OnEnable()
@@ -60,38 +59,37 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        characterController.Move(direction.x, false, jumping);
-        characterController.shoot(shooting);
-        characterController.shootGrenade(shootingGrenade);
-        characterController.aimingDirection = aimDirection;
+        characterController.Move(direction.x, false, jumping); // Move the character based on input
+        characterController.shoot(shooting); // Handle shooting
+        characterController.shootGrenade(shootingGrenade); // Handle grenade shooting
+        characterController.aimingDirection = aimDirection; // Set the aiming direction
     }
 
     private void OnMovementPerformed(InputAction.CallbackContext ctx)
     {
-        direction = ctx.ReadValue<Vector2>();
-
+        direction = ctx.ReadValue<Vector2>(); // Get horizontal and vertical movement
     }
 
     private void OnJumpPerformed(InputAction.CallbackContext ctx)
     {
-        jumping = ctx.performed;
-
-
+        jumping = ctx.performed; // Jumping state
     }
 
     private void OnAimPerformed(InputAction.CallbackContext ctx)
     {
-        aimDirection = ctx.ReadValue<Vector2>();
-
-
+        // Calculate the aiming direction based on mouse position
+        Vector2 mousePos = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        aimDirection = mousePos - (Vector2)transform.position; // Direction from player to mouse
+        aimDirection.Normalize(); // Normalize the direction to avoid scaling the aim
     }
 
     private void OnShootPerformed(InputAction.CallbackContext ctx)
     {
-        shooting = ctx.performed;
+        shooting = ctx.performed; // Shooting state
     }
+
     private void OnShootGrenadePerformed(InputAction.CallbackContext ctx)
     {
-        shootingGrenade = ctx.performed;
+        shootingGrenade = ctx.performed; // Grenade shooting state
     }
 }
